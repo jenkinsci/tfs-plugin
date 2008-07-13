33c9027103c8bebfa735c407e0d5192742be7f90
redsolo <redsolo@dev.java.net>
2008-07-13 20:55:23 +0000

    [tfs] Added parsing of retrieved files and preview argument

--- src/main/java/hudson/plugins/tfs/commands/UpdateWorkfolderCommand.java
+++ src/main/java/hudson/plugins/tfs/commands/UpdateWorkfolderCommand.java
@@ -1,13 +1,30 @@
 package hudson.plugins.tfs.commands;
 
-import hudson.plugins.tfs.commands.Command;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import hudson.plugins.tfs.util.MaskedArgumentListBuilder;
 
-public class UpdateWorkfolderCommand implements Command {
+public class UpdateWorkfolderCommand implements ParseableCommand<List<String>> {
+    private static final Pattern ITEM_PATTERN = Pattern.compile("\\w+\\s+(.*)");
+    private static final Pattern PATH_PATTERN = Pattern.compile("(.+):");
+    
     private final String workFolder;
+    private final boolean preview;
+
+    public UpdateWorkfolderCommand(String workFolder, boolean preview) {
+        this.workFolder = workFolder;
+        this.preview = preview;
+    }
 
     public UpdateWorkfolderCommand(String workFolder) {
-        this.workFolder = workFolder;
+        this(workFolder, false);
     }
 
     public MaskedArgumentListBuilder getArguments() {
@@ -15,6 +32,32 @@ public class UpdateWorkfolderCommand implements Command {
         arguments.add("get");
         arguments.add(workFolder);
         arguments.add("/recursive");
+        if (preview) {
+            arguments.add("/preview");
+        }
         return arguments;
+    }
+
+    public List<String> parse(Reader r) throws ParseException, IOException {
+        BufferedReader reader = new BufferedReader(r);
+        List<String> list = new ArrayList<String>();
+        
+        String line = reader.readLine();
+        String lastPath = null;
+        while (line != null) {
+            Matcher matcher = PATH_PATTERN.matcher(line);
+            if (matcher.matches()) {
+                lastPath = matcher.group(1);
+            } else {                
+                if (lastPath != null) {
+                    matcher = ITEM_PATTERN.matcher(line);
+                    if (matcher.matches()) {
+                        list.add(lastPath + "\\" + matcher.group(1));
+                    }
+                }
+            }            
+            line = reader.readLine();
+        }
+        return list;
     }
 }
--- src/test/java/hudson/plugins/tfs/commands/UpdateWorkfolderCommandTest.java
+++ src/test/java/hudson/plugins/tfs/commands/UpdateWorkfolderCommandTest.java
@@ -2,6 +2,10 @@ package hudson.plugins.tfs.commands;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
+import java.io.StringReader;
+import java.util.List;
+
 import hudson.plugins.tfs.commands.UpdateWorkfolderCommand;
 import hudson.plugins.tfs.util.MaskedArgumentListBuilder;
 
@@ -15,5 +19,47 @@ public class UpdateWorkfolderCommandTest {
         MaskedArgumentListBuilder arguments = new UpdateWorkfolderCommand("localPath").getArguments();
         assertNotNull("Arguments were null", arguments);
         assertEquals("get localPath /recursive", arguments.toStringWithQuote());
-    } 
+    }
+
+    @Test
+    public void assertPreviewArgument() {
+        MaskedArgumentListBuilder arguments = new UpdateWorkfolderCommand("localPath", true).getArguments();
+        assertNotNull("Arguments were null", arguments);
+        assertEquals("get localPath /recursive /preview", arguments.toStringWithQuote());
+    }
+
+    @Test
+    public void assertEmptyListWithEmptyOutput() throws Exception {
+        UpdateWorkfolderCommand command = new UpdateWorkfolderCommand(".");
+        List<String> list = command.parse(new StringReader(""));
+        assertEquals("Number of files was incorrect", 0, list.size());
+    }
+
+    @Test
+    public void assertEmptyListWithNoChangesOutput() throws Exception {
+        UpdateWorkfolderCommand command = new UpdateWorkfolderCommand(".");
+        List<String> list = command.parse(new StringReader("All files are up to date.\n"));
+        assertEquals("Number of files was incorrect", 0, list.size());
+    }    
+    
+    @Test
+    public void assertChangesWithChangeOutput() throws Exception {
+        StringReader reader = new StringReader(
+                "Getting tfsandbox_ms\" +" +
+                "\n" +
+                "C:\\Projects\\tfs\\tfsandbox_ms:\n" +
+                "Getting path1\n" +
+                "Getting path with space\n" +
+                "Getting readme.txt\n" +
+                "\n" +
+                "C:\\Projects\\tfs\\tfsandbox_ms\\path1:\n" +
+                "Getting pom2.xml\n");
+        UpdateWorkfolderCommand command = new UpdateWorkfolderCommand(".");
+        List<String> list = command.parse(reader);
+        assertEquals("Number of files was incorrect", 4, list.size());
+        assertEquals("File name was incorrect", "C:\\Projects\\tfs\\tfsandbox_ms\\path1", list.get(0));
+        assertEquals("File name was incorrect", "C:\\Projects\\tfs\\tfsandbox_ms\\path with space", list.get(1));
+        assertEquals("File name was incorrect", "C:\\Projects\\tfs\\tfsandbox_ms\\readme.txt", list.get(2));
+        assertEquals("File name was incorrect", "C:\\Projects\\tfs\\tfsandbox_ms\\path1\\pom2.xml", list.get(3));
+    }
 }
