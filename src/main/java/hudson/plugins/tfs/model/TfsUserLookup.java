509c4971151de5680fece68bd007930b7d8748fb
Olivier Dagenais <olivier.dagenais@gmail.com>
2013-10-16 01:51:40 +0000

    Add logging when looking up users.

--- src/main/java/hudson/plugins/tfs/model/TfsUserLookup.java
+++ src/main/java/hudson/plugins/tfs/model/TfsUserLookup.java
@@ -1,6 +1,7 @@
 package hudson.plugins.tfs.model;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import com.microsoft.tfs.core.clients.webservices.IIdentityManagementService;
 import com.microsoft.tfs.core.clients.webservices.IdentitySearchFactor;
@@ -13,6 +14,8 @@ import hudson.tasks.Mailer;
 
 public class TfsUserLookup implements UserLookup {
 
+    private static final Logger LOGGER = Logger.getLogger(TfsUserLookup.class.getName());
+
     private final IIdentityManagementService ims;
     
     public TfsUserLookup(IIdentityManagementService ims) {
@@ -23,9 +26,10 @@ public class TfsUserLookup implements UserLookup {
      * @param accountName Windows NT account name: domain\alias.
      */
     public User find(String accountName) {
+        LOGGER.fine("Looking up Jenkins user for TFS account '%s'.");
         final User jenkinsUser = User.get(accountName);
         Mailer.UserProperty mailerProperty = jenkinsUser.getProperty(Mailer.UserProperty.class);
-        if (mailerProperty == null || mailerProperty.getAddress() == null) {
+        if (mailerProperty == null || mailerProperty.getAddress() == null || mailerProperty.getAddress().length() == 0) {
             final TeamFoundationIdentity tfsUser = ims.readIdentity(
                 IdentitySearchFactor.ACCOUNT_NAME,
                 accountName,
@@ -36,15 +40,20 @@ public class TfsUserLookup implements UserLookup {
                 final String displayName = tfsUser.getDisplayName();
                 jenkinsUser.setFullName(displayName);
                 final String emailAddress = (String) tfsUser.getProperty("Mail");
-                // TODO: What should we do if an e-mail address is not configured?  Send to a default account??
                 if (emailAddress != null) {
                     mailerProperty = new Mailer.UserProperty(emailAddress);
                     try {
                         jenkinsUser.addProperty(mailerProperty);
                     } catch (IOException e) {
-                        // ignore
+                        LOGGER.warning(String.format("Unable to save Jenkins account for TFS user '%s'.", accountName));
                     }
                 }
+                else {
+                    LOGGER.info(String.format("TFS user '%s' did not have an e-mail address configured.", accountName));
+                }
+            }
+            else {
+                LOGGER.warning(String.format("Unable to find TFS user '%s'.", accountName));
             }
         }
         return jenkinsUser;
