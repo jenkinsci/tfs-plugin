00c8c28a876a33e3082fdc054dd19454bde8ea96
Oli Dagenais <olivida@microsoft.com>
2015-06-30 22:27:25 -0400

    Extract the AbstractIntegrationTest class.

---
+++ src/test/java/hudson/plugins/tfs/AbstractIntegrationTest.java
@@ -0,0 +1,23 @@
+package hudson.plugins.tfs;
+
+import org.apache.http.client.utils.URIUtils;
+import org.junit.Assert;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public abstract class AbstractIntegrationTest {
+    /**
+     * Creates a string representing the URL to a default TFS server installation, based on the
+     * <code>tfs_server_name</code> property.
+     *
+     * @return a string of the form <code>http://${tfs_server_name}:8080/tfs</code>
+     * @throws URISyntaxException
+     */
+    public String buildTfsServerUrl() throws URISyntaxException {
+        final String tfs_server_name = hudson.Util.fixEmptyAndTrim(System.getProperty("tfs_server_name"));
+        Assert.assertNotNull("The 'tfs_server_name' property was not provided a [non-empty] value.", tfs_server_name);
+        final URI serverUri = URIUtils.createURI("http", tfs_server_name, 8080, "tfs", null, null);
+        return serverUri.toString();
+    }
+}
--- src/test/java/hudson/plugins/tfs/TeamFoundationServerScmIntegrationTest.java
+++ src/test/java/hudson/plugins/tfs/TeamFoundationServerScmIntegrationTest.java
@@ -1,14 +1,11 @@
 package hudson.plugins.tfs;
 
-import hudson.Util;
 import hudson.util.Secret;
-import org.apache.http.client.utils.URIUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
@@ -16,7 +13,7 @@ import java.net.URISyntaxException;
  * These are so-called integration (L2) tests.
  */
 @Category(IntegrationTests.class)
-public class TeamFoundationServerScmIntegrationTest {
+public class TeamFoundationServerScmIntegrationTest extends AbstractIntegrationTest {
 
     private TeamFoundationServerScm scm;
 
@@ -24,20 +21,6 @@ public class TeamFoundationServerScmIntegrationTest {
     public void connectToTfs() throws URISyntaxException {
         final String serverUrl = buildTfsServerUrl();
         scm = new TeamFoundationServerScm(serverUrl, "projectPath", "localPath", false, "workspaceName", null, (Secret) null);
-    }
-
-    /**
-     * Creates a string representing the URL to a default TFS server installation, based on the
-     * <code>tfs_server_name</code> property.
-     *
-     * @return a string of the form <code>http://${tfs_server_name}:8080/tfs</code>
-     * @throws URISyntaxException
-     */
-    public String buildTfsServerUrl() throws URISyntaxException {
-        final String tfs_server_name = Util.fixEmptyAndTrim(System.getProperty("tfs_server_name"));
-        Assert.assertNotNull("The 'tfs_server_name' property was not provided a [non-empty] value.", tfs_server_name);
-        final URI serverUri = URIUtils.createURI("http", tfs_server_name, 8080, "tfs", null, null);
-        return serverUri.toString();
     }
 
     @Test
