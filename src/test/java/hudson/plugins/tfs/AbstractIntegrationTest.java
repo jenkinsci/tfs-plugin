1112d8db456392e0471f479eacdebaf1f4d7b68e
Oli Dagenais <olivida@microsoft.com>
2015-07-07 12:34:57 -0400

    Make method static for better re-use.

--- src/test/java/hudson/plugins/tfs/AbstractIntegrationTest.java
+++ src/test/java/hudson/plugins/tfs/AbstractIntegrationTest.java
@@ -14,7 +14,7 @@ public abstract class AbstractIntegrationTest {
      * @return a string of the form <code>http://${tfs_server_name}:8080/tfs</code>
      * @throws URISyntaxException
      */
-    public String buildTfsServerUrl() throws URISyntaxException {
+    public static String buildTfsServerUrl() throws URISyntaxException {
         final String tfs_server_name = hudson.Util.fixEmptyAndTrim(System.getProperty("tfs_server_name"));
         Assert.assertNotNull("The 'tfs_server_name' property was not provided a [non-empty] value.", tfs_server_name);
         final URI serverUri = URIUtils.createURI("http", tfs_server_name, 8080, "tfs", null, null);
