0b4934349efc4777bc05682b43488d175e2b85cd
Oli Dagenais <olivida@microsoft.com>
2015-07-07 15:46:45 -0400

    Add determinePathInTfvcForTestCase() helper.

--- src/test/java/hudson/plugins/tfs/AbstractIntegrationTest.java
+++ src/test/java/hudson/plugins/tfs/AbstractIntegrationTest.java
@@ -2,11 +2,15 @@ package hudson.plugins.tfs;
 
 import org.apache.maven.wagon.providers.http.httpclient.client.utils.URIUtils;
 import org.junit.Assert;
+import org.junit.runner.Description;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
 public abstract class AbstractIntegrationTest {
+
+    public static final String TeamProjectPrefix = "$/FunctionalTests";
+
     /**
      * Creates a string representing the URL to a default TFS server installation, based on the
      * <code>tfs_server_name</code> property.
@@ -19,5 +23,19 @@ public abstract class AbstractIntegrationTest {
         Assert.assertNotNull("The 'tfs_server_name' property was not provided a [non-empty] value.", tfs_server_name);
         final URI serverUri = URIUtils.createURI("http", tfs_server_name, 8080, "tfs", null, null);
         return serverUri.toString();
+    }
+
+    /**
+     * Creates a string representing a path in TFVC where the specified {@param testDescription}
+     * will perform its work.
+     *
+     * @param testDescription metadata about the currently executing test method.
+     * @return a string that looks like <code>$/FunctionalTests/TestClass/testMethod</code>
+     */
+    public static String determinePathInTfvcForTestCase(Description testDescription) {
+        final Class clazz = testDescription.getTestClass();
+        final String testClassName = clazz.getSimpleName();
+        final String testCaseName = testDescription.getMethodName();
+        return TeamProjectPrefix + "/" + testClassName + "/" + testCaseName;
     }
 }
