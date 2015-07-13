7d6709e3cfab9a219e1950d8e57a9f015410ddbb
Oli Dagenais <olivida@microsoft.com>
2015-07-13 12:46:44 -0400

    Add pokeValue() overload for File + test.

--- src/test/java/hudson/plugins/tfs/util/XmlHelperTest.java
+++ src/test/java/hudson/plugins/tfs/util/XmlHelperTest.java
@@ -1,5 +1,7 @@
 package hudson.plugins.tfs.util;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.w3c.dom.Document;
@@ -12,6 +14,12 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class XmlHelperTest {
 
@@ -42,4 +50,39 @@ public class XmlHelperTest {
         Assert.assertEquals("42", node.getTextContent());
     }
 
+    @Test public void pokeValue_File() throws Exception {
+        final Class<? extends XmlHelperTest> clazz = this.getClass();
+        final String resourceBase = clazz.getSimpleName() + "/pokeValue_File/";
+        final URL inputUrl = clazz.getResource(resourceBase + "input.xml");
+        File tmp = null;
+        BufferedReader expectedReader = null, actualReader = null;
+
+        try {
+            tmp = File.createTempFile("XmlHelperTest", "xml");
+            FileUtils.copyURLToFile(inputUrl, tmp);
+
+            XmlHelper.pokeValue(tmp, "/build/timestamp", "42");
+
+            final URL expectedUrl = clazz.getResource(resourceBase + "expected.xml");
+            expectedReader = new BufferedReader(new InputStreamReader(expectedUrl.openStream()));
+            actualReader = new BufferedReader(new FileReader(tmp));
+            assertReaders(expectedReader, actualReader);
+        } finally {
+            FileUtils.deleteQuietly(tmp);
+            IOUtils.closeQuietly(expectedReader);
+            IOUtils.closeQuietly(actualReader);
+        }
+    }
+
+    /* Adapted from http://stackoverflow.com/a/466854/ */
+    public static void assertReaders(final BufferedReader expected, final BufferedReader actual)
+            throws IOException {
+        String expectedLine;
+        while ((expectedLine = expected.readLine()) != null) {
+            final String actualLine = actual.readLine();
+            Assert.assertNotNull("Expected had more lines than the actual.", actualLine);
+            Assert.assertEquals(expectedLine, actualLine);
+        }
+        Assert.assertNull("Actual had more lines than the expected.", actual.readLine());
+    }
 }
