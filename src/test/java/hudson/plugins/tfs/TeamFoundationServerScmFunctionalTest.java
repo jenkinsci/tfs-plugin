07d3ee7bcc5f7f8d56609d7066f1d5d789fb24c3
Oli Dagenais <olivida@microsoft.com>
2015-07-13 21:19:17 -0400

    Add oldPollingFallback functional test.
    Helps establish a baseline for behaviour I noticed when a build failed
    during checkout() before the plugin could contribute an
    SCMRevisionState.

--- src/test/java/hudson/plugins/tfs/TeamFoundationServerScmFunctionalTest.java
+++ src/test/java/hudson/plugins/tfs/TeamFoundationServerScmFunctionalTest.java
@@ -1,6 +1,21 @@
 package hudson.plugins.tfs;
 
+import hudson.model.Project;
+import hudson.model.TaskListener;
+import hudson.plugins.tfs.util.XmlHelper;
+import hudson.scm.PollingResult;
+import jenkins.model.Jenkins;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.jvnet.hudson.test.JenkinsRecipe;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.recipes.LocalData;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Tests that exercise real-world functionality, using temporary Jenkins instances.
@@ -9,4 +24,51 @@ import org.junit.experimental.categories.Category;
  */
 @Category(IntegrationTests.class)
 public class TeamFoundationServerScmFunctionalTest {
+
+    @Rule public JenkinsRule j = new JenkinsRule();
+
+    /*
+     * If there's no SCMRevisionState present, we revert to old-school polling,
+     * using the timestamp of the last build to see if there have been any changes in TFVC,
+     * at the project's path, since the specified time.
+     *
+     * Even though the @EndToEndTfs annotation caused some commits,
+     * the OldPollingFallback runner poked the current time (after said commits)
+     * in the build.xml, which should cause polling to not find any changes.
+     */
+    @LocalData
+    @EndToEndTfs(OldPollingFallback.class)
+    @Test public void oldPollingFallback() throws IOException {
+        final Jenkins jenkins = j.jenkins;
+        final List<Project> projects = jenkins.getProjects();
+        final Project project = projects.get(0);
+        final TaskListener taskListener = j.createTaskListener();
+
+        final PollingResult actual = project.poll(taskListener);
+
+        Assert.assertEquals(PollingResult.NO_CHANGES, actual);
+    }
+
+    /**
+     * Injects the current time in milliseconds into the <code>/build/timestamp</code> element
+     * of the last <code>build.xml</code>.
+     */
+    public static class OldPollingFallback extends JenkinsRecipe.Runner<EndToEndTfs> {
+
+        private static final String jobFolder = "jobs/oldPollingFallback/";
+
+        @Override public void decorateHome(final JenkinsRule jenkinsRule, final File home)
+                throws Exception {
+            final String configXmlPath = jobFolder + "config.xml";
+            final File configXmlFile = new File(home, configXmlPath);
+            final String tfsServerUrl = AbstractIntegrationTest.buildTfsServerUrl();
+            XmlHelper.pokeValue(configXmlFile, "/project/scm/serverUrl", tfsServerUrl);
+
+            final String lastBuildXmlPath = jobFolder + "builds/2015-07-10_12-11-34/build.xml";
+            final File lastBuildXmlFile = new File(home, lastBuildXmlPath);
+            final long rightNowMilliseconds = System.currentTimeMillis();
+            final String value = String.valueOf(rightNowMilliseconds);
+            XmlHelper.pokeValue(lastBuildXmlFile, "/build/timestamp", value);
+        }
+    }
 }
