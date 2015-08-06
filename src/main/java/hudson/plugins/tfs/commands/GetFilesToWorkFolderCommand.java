99716578ba8c670573cfb23d3b7693a2e4d5378e
Oli Dagenais <olivida@microsoft.com>
2015-08-06 13:37:22 -0400

    parse() only used by tests; no uses of PC<T> left.
    Project.getFiles() discards the results of the
    GetFilesToWorkFolderCommand and thus the interface implementation
    wasn't [any longer] necessary, and since we removed the last
    implementation, the ParseableCommand<T> interface wasn't necessary.

--- src/main/java/hudson/plugins/tfs/commands/GetFilesToWorkFolderCommand.java
+++ src/main/java/hudson/plugins/tfs/commands/GetFilesToWorkFolderCommand.java
@@ -1,21 +1,10 @@
 package hudson.plugins.tfs.commands;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import hudson.plugins.tfs.model.Server;
 import hudson.plugins.tfs.util.MaskedArgumentListBuilder;
 
-public class GetFilesToWorkFolderCommand extends AbstractCommand implements ParseableCommand<List<String>> {
-    private static final Pattern ITEM_PATTERN = Pattern.compile("\\w+\\s+(.*)");
-    private static final Pattern PATH_PATTERN = Pattern.compile("(.+):");
-    
+public class GetFilesToWorkFolderCommand extends AbstractCommand {
+
     private final String workFolder;
     private final String versionSpec;
 
@@ -36,28 +25,5 @@ public class GetFilesToWorkFolderCommand extends AbstractCommand implements Pars
         arguments.add("-noprompt");
         addLoginArgument(arguments);
         return arguments;
-    }
-
-    public List<String> parse(Reader r) throws ParseException, IOException {
-        BufferedReader reader = new BufferedReader(r);
-        List<String> list = new ArrayList<String>();
-        
-        String line = reader.readLine();
-        String lastPath = null;
-        while (line != null) {
-            Matcher matcher = PATH_PATTERN.matcher(line);
-            if (matcher.matches()) {
-                lastPath = matcher.group(1);
-            } else {                
-                if (lastPath != null) {
-                    matcher = ITEM_PATTERN.matcher(line);
-                    if (matcher.matches()) {
-                        list.add(lastPath + "\\" + matcher.group(1));
-                    }
-                }
-            }            
-            line = reader.readLine();
-        }
-        return list;
     }
 }
--- src/test/java/hudson/plugins/tfs/commands/GetFilesToWorkFolderCommandTest.java
+++ src/test/java/hudson/plugins/tfs/commands/GetFilesToWorkFolderCommandTest.java
@@ -3,9 +3,6 @@ package hudson.plugins.tfs.commands;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-import java.io.StringReader;
-import java.util.List;
-
 import hudson.plugins.tfs.model.Server;
 import hudson.plugins.tfs.util.MaskedArgumentListBuilder;
 
@@ -23,40 +20,5 @@ public class GetFilesToWorkFolderCommandTest {
         MaskedArgumentListBuilder arguments = new GetFilesToWorkFolderCommand(config, "localPath", "C100").getArguments();
         assertNotNull("Arguments were null", arguments);
         assertEquals("get localPath -recursive -version:C100 -noprompt -login:snd\\user_cp,password", arguments.toStringWithQuote());
-    }
-
-    @Test
-    public void assertEmptyListWithEmptyOutput() throws Exception {
-        GetFilesToWorkFolderCommand command = new GetFilesToWorkFolderCommand(mock(Server.class), ".", "T");
-        List<String> list = command.parse(new StringReader(""));
-        assertEquals("Number of files was incorrect", 0, list.size());
-    }
-
-    @Test
-    public void assertEmptyListWithNoChangesOutput() throws Exception {
-        GetFilesToWorkFolderCommand command = new GetFilesToWorkFolderCommand(mock(Server.class), ".", "T");
-        List<String> list = command.parse(new StringReader("All files are up to date.\n"));
-        assertEquals("Number of files was incorrect", 0, list.size());
-    }    
-    
-    @Test
-    public void assertChangesWithChangeOutput() throws Exception {
-        StringReader reader = new StringReader(
-                "Getting tfsandbox_ms\" +" +
-                "\n" +
-                "C:\\Projects\\tfs\\tfsandbox_ms:\n" +
-                "Getting path1\n" +
-                "Getting path with space\n" +
-                "Getting readme.txt\n" +
-                "\n" +
-                "C:\\Projects\\tfs\\tfsandbox_ms\\path1:\n" +
-                "Getting pom2.xml\n");
-        GetFilesToWorkFolderCommand command = new GetFilesToWorkFolderCommand(mock(Server.class), ".", "T");
-        List<String> list = command.parse(reader);
-        assertEquals("Number of files was incorrect", 4, list.size());
-        assertEquals("File name was incorrect", "C:\\Projects\\tfs\\tfsandbox_ms\\path1", list.get(0));
-        assertEquals("File name was incorrect", "C:\\Projects\\tfs\\tfsandbox_ms\\path with space", list.get(1));
-        assertEquals("File name was incorrect", "C:\\Projects\\tfs\\tfsandbox_ms\\readme.txt", list.get(2));
-        assertEquals("File name was incorrect", "C:\\Projects\\tfs\\tfsandbox_ms\\path1\\pom2.xml", list.get(3));
     }
 }
