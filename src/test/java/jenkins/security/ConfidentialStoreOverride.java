b4ba9f1b15fd912d6bdececa4e0cdaf570efff61
Oli Dagenais <olivida@microsoft.com>
2016-07-11 08:42:45 -0400

    Add ConfidentialStoreOverride skeleton
    Newer versions of Jenkins have an even more elaborate set of encryption
    keys to protect the secrets, which means we'll need a second override.

---
+++ src/test/java/jenkins/security/ConfidentialStoreOverride.java
@@ -0,0 +1,11 @@
+package jenkins.security;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class ConfidentialStoreOverride implements Closeable {
+
+    public void close() throws IOException {
+        // TODO: implement
+    }
+}
