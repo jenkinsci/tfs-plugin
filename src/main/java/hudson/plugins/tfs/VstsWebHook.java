05f728f298ca40be1c75f69f1260b2b10ef067e3
Oli Dagenais <olivida@microsoft.com>
2016-07-13 04:39:53 -0400

    Expand VstsWebHook to use ResponseContributor

--- src/main/java/hudson/plugins/tfs/VstsWebHook.java
+++ src/main/java/hudson/plugins/tfs/VstsWebHook.java
@@ -2,15 +2,26 @@ package hudson.plugins.tfs;
 
 import hudson.Extension;
 import hudson.model.UnprotectedRootAction;
+import hudson.plugins.git.GitStatus;
 import hudson.plugins.tfs.util.StringBodyParameter;
 import org.kohsuke.stapler.HttpResponse;
 import org.kohsuke.stapler.HttpResponses;
 import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
 import org.kohsuke.stapler.interceptor.RequirePOST;
 
 import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+
 import javax.annotation.Nonnull;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -43,9 +54,15 @@ public class VstsWebHook implements UnprotectedRootAction {
             final HttpServletRequest request,
             @QueryParameter(required = true) @Nonnull final VstsHookEventName event,
             @StringBodyParameter @Nonnull final String body) {
+        LOGGER.log(Level.FINER, "{}/?event={}\n{}", new String[]{URL_NAME, event.name(), body});
+        final Object parsedBody = event.parse(body);
+        List<? extends GitStatus.ResponseContributor> contributors = null;
         switch (event) {
             case PING:
-                return HttpResponses.plainText("Pong from the Jenkins TFS plugin! Here's your body:\n" + body);
+                final String message = "Pong from the Jenkins TFS plugin! Here's your body:\n" + parsedBody;
+                final GitStatus.MessageResponseContributor contributor;
+                contributor = new GitStatus.MessageResponseContributor(message);
+                contributors = Collections.singletonList(contributor);
             case BUILD_COMPLETED:
                 break;
             case GIT_CODE_PUSHED:
@@ -59,6 +76,26 @@ public class VstsWebHook implements UnprotectedRootAction {
             case RELEASE_CREATED:
                 break;
         }
-        return HttpResponses.error(SC_BAD_REQUEST, "Not implemented");
+        if (contributors == null) {
+            return HttpResponses.error(SC_BAD_REQUEST, "Not implemented");
+        }
+        else {
+            final List<? extends GitStatus.ResponseContributor> finalContributors = contributors;
+            return new HttpResponse() {
+                public void generateResponse(StaplerRequest req, StaplerResponse rsp, Object node)
+                        throws IOException, ServletException {
+                    rsp.setStatus(SC_OK);
+                    rsp.setContentType("text/plain");
+                    for (GitStatus.ResponseContributor c : finalContributors) {
+                        c.addHeaders(req, rsp);
+                    }
+                    PrintWriter w = rsp.getWriter();
+                    for (GitStatus.ResponseContributor c : finalContributors) {
+                        c.writeBody(req, rsp, w);
+                    }
+                }
+            };
+
+        }
     }
 }
