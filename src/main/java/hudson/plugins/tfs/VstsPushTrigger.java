2e720bac23bd695484ac1f8c4689ca25079869aa
Oli Dagenais <olivida@microsoft.com>
2016-07-13 00:53:01 -0400

    Implement VstsPushTrigger polling & queueing

--- src/main/java/hudson/plugins/tfs/VstsPushTrigger.java
+++ src/main/java/hudson/plugins/tfs/VstsPushTrigger.java
@@ -1,17 +1,27 @@
 package hudson.plugins.tfs;
 
 import hudson.Extension;
+import hudson.Util;
 import hudson.model.Action;
+import hudson.model.CauseAction;
 import hudson.model.Item;
 import hudson.model.Job;
+import hudson.model.queue.QueueTaskFuture;
 import hudson.triggers.Trigger;
 import hudson.triggers.TriggerDescriptor;
+import hudson.util.StreamTaskListener;
 import jenkins.model.ParameterizedJobMixIn;
 import jenkins.triggers.SCMTriggerItem;
 import org.kohsuke.stapler.DataBoundConstructor;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.text.DateFormat;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -25,9 +35,95 @@ public class VstsPushTrigger extends Trigger<Job<?, ?>> {
     public VstsPushTrigger() {
     }
 
-    public void execute() {
-        // TODO: run polling
-        // TODO: if polling says there are changes, create a VstsPushCause and scheduleBuild()
+    public void execute(final String pushedBy) {
+        // TODO: Consider executing the poll + queue asynchronously
+        final Runner runner = new Runner(pushedBy);
+        runner.run();
+    }
+
+    public File getLogFile() {
+        return new File(job.getRootDir(), "vsts-polling.log");
+    }
+
+    // TODO: This was inspired by SCMTrigger.Runner; it would be worth extracting something for re-use
+    public class Runner implements Runnable {
+
+        private final String pushedBy;
+
+        public Runner(final String pushedBy) {
+            this.pushedBy = pushedBy;
+        }
+
+        private SCMTriggerItem job() {
+            return SCMTriggerItem.SCMTriggerItems.asSCMTriggerItem(job);
+        }
+
+        private boolean runPolling() {
+            final String failedToRecord = "Failed to record SCM polling for " + job;
+            try {
+                final StreamTaskListener listener = new StreamTaskListener(getLogFile());
+
+                try {
+                    final PrintStream logger = listener.getLogger();
+                    final long startTimeMillis = System.currentTimeMillis();
+                    final Date date = new Date(startTimeMillis);
+                    logger.println("Started on "+ DateFormat.getDateTimeInstance().format(date));
+                    final boolean result = job().poll(listener).hasChanges();
+                    final long endTimeMillis = System.currentTimeMillis();
+                    logger.println("Done. Took "+ Util.getTimeSpanString(endTimeMillis - startTimeMillis));
+                    if (result) {
+                        logger.println("Changes found");
+                    }
+                    else {
+                        logger.println("No changes");
+                    }
+                    return result;
+                }
+                catch (final Error e) {
+                    e.printStackTrace(listener.error(failedToRecord));
+                    LOGGER.log(Level.SEVERE, failedToRecord,e);
+                    throw e;
+                }
+                catch (final RuntimeException e) {
+                    e.printStackTrace(listener.error(failedToRecord));
+                    LOGGER.log(Level.SEVERE, failedToRecord,e);
+                    throw e;
+                }
+                finally {
+                    listener.close();
+                }
+            }
+            catch (final IOException e) {
+                LOGGER.log(Level.SEVERE, failedToRecord, e);
+                return false;
+            }
+        }
+
+        @Override
+        public void run() {
+            if (runPolling()) {
+                final String changesDetected = "SCM changes detected in " + job.getFullDisplayName() + ".";
+                final SCMTriggerItem p = job();
+                final String name = " #" + p.getNextBuildNumber();
+                VstsPushCause cause;
+                try {
+                    cause = new VstsPushCause(getLogFile(), pushedBy);
+                }
+                catch (IOException e) {
+                    LOGGER.log(Level.WARNING, "Failed to parse the polling log", e);
+                    cause = new VstsPushCause(pushedBy);
+                }
+                final CauseAction causeAction = new CauseAction(cause);
+                final int quietPeriod = p.getQuietPeriod();
+                final QueueTaskFuture<?> queueTaskFuture = p.scheduleBuild2(quietPeriod, causeAction);
+                if (queueTaskFuture != null) {
+                    LOGGER.info(changesDetected + " Triggering " + name);
+                }
+                else {
+                    LOGGER.info(changesDetected + " Job is already in the queue");
+                }
+            }
+        }
     }
 
     @Override
