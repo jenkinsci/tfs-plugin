62d0490bf81868b53807cdcf483da231a702dddc
Oli Dagenais <olivida@microsoft.com>
2016-07-15 10:19:20 -0400

    Populate credentials list based on collection host

--- src/main/java/hudson/plugins/tfs/VstsCollectionConfiguration.java
+++ src/main/java/hudson/plugins/tfs/VstsCollectionConfiguration.java
@@ -1,15 +1,22 @@
 package hudson.plugins.tfs;
 
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import com.cloudbees.plugins.credentials.domains.HostnameRequirement;
 import hudson.Extension;
 import hudson.model.AbstractDescribableImpl;
 import hudson.model.Descriptor;
+import hudson.security.ACL;
 import hudson.util.FormValidation;
 import hudson.util.ListBoxModel;
+import jenkins.model.Jenkins;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 public class VstsCollectionConfiguration extends AbstractDescribableImpl<VstsCollectionConfiguration> {
 
@@ -63,8 +70,31 @@ public class VstsCollectionConfiguration extends AbstractDescribableImpl<VstsCol
         public ListBoxModel doFillCredentialsIdItems(
             @QueryParameter final String collectionUrl) {
 
-            // TODO: populate listbox with credentials
-            return new ListBoxModel();
+            final Jenkins jenkins = Jenkins.getInstance();
+
+            String hostName = null;
+            try {
+                final URL url = new URL(collectionUrl);
+                hostName = url.getHost();
+            }
+            catch (final MalformedURLException ignored) {
+            }
+
+            if (hostName == null || !jenkins.hasPermission(Jenkins.ADMINISTER)) {
+                return new ListBoxModel();
+            }
+
+            final HostnameRequirement requirement = new HostnameRequirement(hostName);
+            final List<StandardUsernamePasswordCredentials> matches =
+                CredentialsProvider.lookupCredentials(
+                    StandardUsernamePasswordCredentials.class,
+                    jenkins,
+                    ACL.SYSTEM,
+                    requirement
+                );
+            return new StandardListBoxModel()
+                    .withEmptySelection()
+                    .withAll(matches);
         }
     }
 }
