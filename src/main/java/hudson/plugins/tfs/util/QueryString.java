3582c9667220a5c6883f7336bc91afbcc839673e
Oli Dagenais <olivida@microsoft.com>
2016-07-15 11:17:28 -0400

    TDD: QueryString parameterized constructor

--- src/main/java/hudson/plugins/tfs/util/QueryString.java
+++ src/main/java/hudson/plugins/tfs/util/QueryString.java
@@ -6,6 +6,21 @@ package hudson.plugins.tfs.util;
 import java.util.LinkedHashMap;
 
 public class QueryString extends LinkedHashMap<String, String> {
+
+    public QueryString() {
+    }
+
+    public QueryString(final String... nameValuePairs) {
+        final int length = nameValuePairs.length;
+        if (length % 2 != 0) {
+            final String message = "This QueryString constructor needs an even number of parameters";
+            throw new IllegalArgumentException(message);
+        }
+        for (int i = 0; i < length; i+=2) {
+            put(nameValuePairs[i], nameValuePairs[i + 1]);
+        }
+    }
+
     @Override
     public String toString() {
         return UriHelper.serializeParameters(this);
--- src/test/java/hudson/plugins/tfs/util/QueryStringTest.java
+++ src/test/java/hudson/plugins/tfs/util/QueryStringTest.java
@@ -17,4 +17,27 @@ public class QueryStringTest {
         Assert.assertEquals("answer=42", actual);
     }
 
+    @Test public void constructor_typical() throws Exception {
+        //noinspection MismatchedQueryAndUpdateOfCollection
+        final QueryString cut = new QueryString("answer", "42");
+
+        final String actual = cut.toString();
+
+        Assert.assertEquals("answer=42", actual);
+    }
+
+    @Test public void constructor_twoPairs() throws Exception {
+        //noinspection MismatchedQueryAndUpdateOfCollection
+        final QueryString cut = new QueryString("answer", "42", "question", "whatdoyoug");
+
+        final String actual = cut.toString();
+
+        Assert.assertEquals("answer=42&question=whatdoyoug", actual);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructor_oddParameters() throws Exception {
+        new QueryString("answer");
+    }
+
 }
