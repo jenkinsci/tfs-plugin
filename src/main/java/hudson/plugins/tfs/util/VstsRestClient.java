57f8b40b4c790e0ad581764c8ab1d5e739d56f71
Oli Dagenais <olivida@microsoft.com>
2016-07-18 09:11:39 -0400

    Add createAuthorization() + test

--- src/main/java/hudson/plugins/tfs/util/VstsRestClient.java
+++ src/main/java/hudson/plugins/tfs/util/VstsRestClient.java
@@ -1,5 +1,24 @@
 package hudson.plugins.tfs.util;
 
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import hudson.util.Secret;
+
+import javax.xml.bind.DatatypeConverter;
+import java.nio.charset.Charset;
+
 public class VstsRestClient {
+
+    private static final Charset UTF8 = Charset.forName("UTF-8");
+
+    static String createAuthorization(final StandardUsernamePasswordCredentials credentials) {
+        final String username = credentials.getUsername();
+        final Secret secretPassword = credentials.getPassword();
+        final String password = secretPassword.getPlainText();
+        final String credPair = username + ":" + password;
+        final byte[] credBytes = credPair.getBytes(UTF8);
+        final String base64enc = DatatypeConverter.printBase64Binary(credBytes);
+        final String result = "Basic " + base64enc;
+        return result;
+    }
 
 }
--- src/test/java/hudson/plugins/tfs/util/VstsRestClientTest.java
+++ src/test/java/hudson/plugins/tfs/util/VstsRestClientTest.java
@@ -1,6 +1,12 @@
 package hudson.plugins.tfs.util;
 
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
+import hudson.util.SecretOverride;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -8,4 +14,29 @@ import org.junit.Test;
  */
 public class VstsRestClientTest {
 
+    private SecretOverride secretOverride = null;
+
+    @Before public void setUp() throws Exception {
+        secretOverride = new SecretOverride();
+    }
+
+    @After public void tearDown() throws Exception {
+        if (secretOverride != null) {
+            secretOverride.close();
+        }
+    }
+
+    @Test public void createAuthorization_typical() throws Exception {
+        final String personalAccessToken = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        final StandardUsernamePasswordCredentials creds = new UsernamePasswordCredentialsImpl(
+            CredentialsScope.SYSTEM,
+            "vstsBuildAccount",
+            null,
+            "PAT",
+            personalAccessToken);
+
+        final String actual = VstsRestClient.createAuthorization(creds);
+
+        Assert.assertEquals("Basic UEFUOmFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWE=", actual);
+    }
 }
