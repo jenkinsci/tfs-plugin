6407996125620484b565881074fa739cf129ee20
Oli Dagenais <olivida@microsoft.com>
2016-07-17 22:05:03 -0400

    Add VstsRestClient[Test] skeletons

---
+++ src/main/java/hudson/plugins/tfs/util/VstsRestClient.java
@@ -0,0 +1,5 @@
+package hudson.plugins.tfs.util;
+
+public class VstsRestClient {
+
+}
---
+++ src/test/java/hudson/plugins/tfs/util/VstsRestClientTest.java
@@ -0,0 +1,11 @@
+package hudson.plugins.tfs.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * A class to test {@link VstsRestClient}.
+ */
+public class VstsRestClientTest {
+
+}
