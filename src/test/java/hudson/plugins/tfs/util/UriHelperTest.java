2d963124c8c9e2ecee12ffc30508d94c3bd4d896
Oli Dagenais <olivida@microsoft.com>
2016-07-20 13:25:35 -0400

    Add UriHelper#areSame() with tests

--- src/test/java/hudson/plugins/tfs/util/UriHelperTest.java
+++ src/test/java/hudson/plugins/tfs/util/UriHelperTest.java
@@ -18,6 +18,109 @@ public class UriHelperTest {
         lifeUniverseEverything.put("answer", "42");
     }
 
+    private static void assertSame(final String a, final String b) {
+        areSame(a, b, true);
+    }
+
+    private static void assertNotSame(final String a, final String b) {
+        areSame(a, b, false);
+    }
+
+    private static void areSame(final String a, final String b, final boolean expected) {
+        final URI uriA = a == null ? null : URI.create(a);
+        final URI uriB = b == null ? null : URI.create(b);
+        final String template = "Expected '%s' and '%s' to be considered%s the same.";
+        final String message = String.format(template, a, b, expected ? "" : " NOT");
+        Assert.assertEquals(message, expected, UriHelper.areSame(uriA, uriB));
+        Assert.assertEquals(message, expected, UriHelper.areSame(uriB, uriA));
+    }
+
+
+    @Test public void areSame_bothNull() throws Exception {
+        assertSame(null, null);
+    }
+
+    @Test public void areSame_sameInstance() throws Exception {
+        final URI uri = URI.create("http://one.example.com");
+        Assert.assertTrue(UriHelper.areSame(uri, uri));
+    }
+
+    @Test public void areSame_identity() throws Exception {
+        assertSame("http://one.example.com", "http://one.example.com");
+    }
+
+    @Test public void areSame_endsWithSlash() throws Exception {
+        assertSame("http://one.example.com/", "http://one.example.com");
+    }
+
+    @Test public void areSame_schemeCase() throws Exception {
+        assertSame("http://one.example.com", "HTTP://one.example.com");
+    }
+
+    @Test public void areSame_hostCase() throws Exception {
+        assertSame("http://ONE.example.com", "http://one.example.com");
+    }
+
+    @Test public void areSame_implicitPort() throws Exception {
+        assertSame("http://one.example.com", "http://one.example.com:80");
+    }
+
+    @Test public void areSame_withPathSlash() throws Exception {
+        assertSame("http://one.example.com/path/", "http://one.example.com/path/");
+    }
+
+    @Test public void areSame_withPathWithoutSlash() throws Exception {
+        assertSame("http://one.example.com/path/", "http://one.example.com/path");
+    }
+
+    @Test public void areSame_withPathQuery() throws Exception {
+        assertSame("http://one.example.com/search?q=example", "http://one.example.com/search?q=example");
+    }
+
+    @Test public void areSame_withPathQueryFragment() throws Exception {
+        assertSame("http://one.example.com/search?q=example#top", "http://one.example.com/search?q=example#top");
+    }
+
+
+    @Test public void areSame_oneNull() throws Exception {
+
+        assertNotSame("http://one.example.com/path/", null);
+    }
+
+    @Test public void areSame_differentScheme() throws Exception {
+
+        assertNotSame("http://one.example.com/path/", "https://one.example.com/path/");
+    }
+
+    @Test public void areSame_differentHost() throws Exception {
+
+        assertNotSame("http://one.example.com/path/", "http://two.example.com/path/");
+    }
+
+    @Test public void areSame_differentPort() throws Exception {
+
+        assertNotSame("http://one.example.com/path/", "http://one.example.com:8080/path/");
+    }
+
+    @Test public void areSame_differentPath() throws Exception {
+
+        assertNotSame("http://one.example.com/path/", "http://one.example.com/");
+    }
+
+    @Test public void areSame_differentQuery() throws Exception {
+
+        assertNotSame("http://one.example.com/path?q=example", "http://one.example.com/path?q=different");
+    }
+
+    @Test public void areSame_differentFragment() throws Exception {
+
+        assertNotSame("http://one.example.com/path#top", "http://one.example.com/path#bottom");
+    }
+
+    @Test public void areSame_differentFragmentAfterQuery() throws Exception {
+
+        assertNotSame("http://one.example.com/path?q=example#top", "http://one.example.com/path?q=example#bottom");
+    }
 
     @Test public void join_uriNoSlash_pathComponents() throws Exception {
         final URI collectionUri = URI.create("https://fabrikam-fiber-inc.visualstudio.com");
