2c8c1253f6454add4470512eecde5146708d3e87
Oli Dagenais <olivida@microsoft.com>
2016-07-22 21:36:20 -0400

    Extract VstsStatus#createFromRun() for re-use

--- src/main/java/hudson/plugins/tfs/VstsCompletedStatusPostBuildAction.java
+++ src/main/java/hudson/plugins/tfs/VstsCompletedStatusPostBuildAction.java
@@ -1,17 +1,12 @@
 package hudson.plugins.tfs;
 
-import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractProject;
-import hudson.model.Result;
 import hudson.model.Run;
 import hudson.model.TaskListener;
-import hudson.plugins.tfs.model.GitCodePushedEventArgs;
-import hudson.plugins.tfs.model.GitStatusState;
-import hudson.plugins.tfs.model.VstsGitStatus;
-import hudson.plugins.tfs.util.VstsRestClient;
+import hudson.plugins.tfs.util.VstsStatus;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Notifier;
@@ -21,10 +16,6 @@ import org.kohsuke.stapler.DataBoundConstructor;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
-import java.net.URI;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * A _Post-Build Action_ that reports the completion status of an associated build to VSTS.
@@ -44,27 +35,7 @@ public class VstsCompletedStatusPostBuildAction extends Notifier implements Simp
             @Nonnull final TaskListener listener
     ) throws InterruptedException, IOException {
         try {
-            // TODO: also add support for a build triggered from a pull request
-            final CommitParameterAction commitParameter = run.getAction(CommitParameterAction.class);
-            final GitCodePushedEventArgs args;
-            if (commitParameter != null) {
-                args = commitParameter.getGitCodePushedEventArgs();
-            }
-            else {
-                // TODO: try to guess based on what we _do_ have (i.e. RevisionParameterAction)
-                return;
-            }
-
-            final URI collectionUri = args.collectionUri;
-            final StandardUsernamePasswordCredentials credentials =
-                    VstsCollectionConfiguration.findCredentialsForCollection(collectionUri);
-            final VstsRestClient client = new VstsRestClient(collectionUri, credentials);
-
-            final VstsGitStatus status = VstsGitStatus.fromRun(run);
-            // TODO: when code is pushed and polling happens, are we sure we built against the requested commit?
-            client.addCommitStatus(args, status);
-
-            // TODO: we could contribute an Action to the run, recording the ID of the status we created
+            VstsStatus.createFromRun(run);
         }
         catch (final Exception e) {
             e.printStackTrace(listener.error("Error while trying to update completion status in VSTS"));
