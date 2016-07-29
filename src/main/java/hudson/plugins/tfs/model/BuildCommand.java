d7b4ac87615f88893e18991ec64bae770b2f64bf
Oli Dagenais <olivida@microsoft.com>
2016-07-29 16:25:36 -0400

    Preliminary build support

--- src/main/java/hudson/plugins/tfs/model/BuildCommand.java
+++ src/main/java/hudson/plugins/tfs/model/BuildCommand.java
@@ -1,10 +1,24 @@
 package hudson.plugins.tfs.model;
 
 import hudson.model.AbstractProject;
+import hudson.model.Action;
+import hudson.model.Cause;
+import hudson.model.CauseAction;
+import hudson.model.Queue;
+import hudson.model.queue.ScheduleResult;
+import hudson.plugins.tfs.CommitParameterAction;
+import hudson.plugins.tfs.PullRequestParameterAction;
+import jenkins.model.Jenkins;
 import jenkins.util.TimeDuration;
 import net.sf.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BuildCommand extends AbstractCommand {
+
+    private static final String TEAM_PARAMETERS = "team-parameters";
+    private static final Action[] EMPTY_ACTION_ARRAY = new Action[0];
 
     public static class Factory implements AbstractCommand.Factory {
         @Override
@@ -30,7 +44,38 @@ public class BuildCommand extends AbstractCommand {
 
     @Override
     public JSONObject perform(final AbstractProject project, final TimeDuration delay, final JSONObject requestPayload) {
-        // TODO: implement
-        return requestPayload;
+        // TODO: detect if a job is parameterized and react appropriately
+        final JSONObject result = new JSONObject();
+
+        final Jenkins jenkins = Jenkins.getInstance();
+        final Queue queue = jenkins.getQueue();
+        final Cause cause = new Cause.UserIdCause();
+        final CauseAction causeAction = new CauseAction(cause);
+        final List<Action> actions = new ArrayList<Action>();
+        actions.add(causeAction);
+
+        if (requestPayload.containsKey(TEAM_PARAMETERS)) {
+            final JSONObject eventArgsJson = requestPayload.getJSONObject(TEAM_PARAMETERS);
+            final CommitParameterAction action;
+            // TODO: improve the payload detection!
+            if (eventArgsJson.containsKey("pullRequestId")) {
+                final PullRequestMergeCommitCreatedEventArgs args = PullRequestMergeCommitCreatedEventArgs.fromJsonObject(eventArgsJson);
+                action = new PullRequestParameterAction(args);
+            }
+            else {
+                final GitCodePushedEventArgs args = GitCodePushedEventArgs.fromJsonObject(eventArgsJson);
+                action = new CommitParameterAction(args);
+            }
+            actions.add(action);
+        }
+
+        final Action[] actionArray = actions.toArray(EMPTY_ACTION_ARRAY);
+        final ScheduleResult scheduleResult = queue.schedule2(project, delay.getTime(), actionArray);
+        final Queue.Item item = scheduleResult.getItem();
+        if (item != null) {
+            result.put("created", jenkins.getRootUrl() + item.getUrl());
+        }
+
+        return result;
     }
 }
