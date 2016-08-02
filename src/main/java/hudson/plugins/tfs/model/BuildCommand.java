b01e8d077f80c814f1e2da3a291b533fc8ed2ffa
Oli Dagenais <olivida@microsoft.com>
2016-08-02 00:08:29 -0400

    Add support for /build/ with legacy forms

--- src/main/java/hudson/plugins/tfs/model/BuildCommand.java
+++ src/main/java/hudson/plugins/tfs/model/BuildCommand.java
@@ -13,13 +13,27 @@ import jenkins.util.TimeDuration;
 import net.sf.json.JSONObject;
 import org.kohsuke.stapler.StaplerRequest;
 
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BuildCommand extends AbstractCommand {
 
     private static final String TEAM_PARAMETERS = "team-parameters";
     private static final Action[] EMPTY_ACTION_ARRAY = new Action[0];
+    protected static final String TEAM_BUILD_PREFIX = "_team-build_";
+    protected static final int TEAM_BUILD_PREFIX_LENGTH = TEAM_BUILD_PREFIX.length();
+    private static final String BUILD_SOURCE_BRANCH = "Build.SourceBranch";
+    private static final String BUILD_REPOSITORY_PROVIDER = "Build.Repository.Provider";
+    private static final String REFS_PULL_SLASH = "refs/pull/";
+    private static final int REFS_PULL_SLASH_LENGTH = REFS_PULL_SLASH.length();
+    private static final String BUILD_REPOSITORY_URI = "Build.Repository.Uri";
+    private static final String SYSTEM_TEAM_PROJECT = "System.TeamProject";
+    private static final String BUILD_SOURCE_VERSION = "Build.SourceVersion";
+    private static final String BUILD_REQUESTED_FOR = "Build.RequestedFor";
+    private static final String SYSTEM_TEAM_FOUNDATION_COLLECTION_URI = "System.TeamFoundationCollectionUri";
 
     public static class Factory implements AbstractCommand.Factory {
         @Override
@@ -89,6 +103,76 @@ public class BuildCommand extends AbstractCommand {
 
     @Override
     public JSONObject perform(final AbstractProject project, final StaplerRequest request, final TimeDuration delay) {
-        throw new IllegalStateException("Not implemented");
+
+        final List<Action> actions = new ArrayList<Action>();
+
+        final HashMap<String, String> teamParameters = new HashMap<String, String>();
+
+        final Map<String, String[]> parameters = request.getParameterMap();
+        for (final Map.Entry<String, String[]> entry : parameters.entrySet()) {
+            final String paramName = entry.getKey();
+            if (paramName.startsWith(TEAM_BUILD_PREFIX)) {
+                final String teamParamName = paramName.substring(TEAM_BUILD_PREFIX_LENGTH);
+                final String[] valueArray = entry.getValue();
+                if (valueArray == null || valueArray.length != 1) {
+                    throw new IllegalArgumentException(String.format("Expected exactly 1 value for parameter '%s'.", teamParamName));
+                }
+                teamParameters.put(teamParamName, valueArray[0]);
+            }
+            else {
+                // TODO: implement when we add support for parameterized builds
+            }
+        }
+
+        if (teamParameters.containsKey(BUILD_REPOSITORY_PROVIDER) && "TfGit".equalsIgnoreCase(teamParameters.get(BUILD_REPOSITORY_PROVIDER))) {
+            final String collectionUriString = teamParameters.get(SYSTEM_TEAM_FOUNDATION_COLLECTION_URI);
+            final URI collectionUri = URI.create(collectionUriString);
+            final String repoUriString = teamParameters.get(BUILD_REPOSITORY_URI);
+            final URI repoUri = URI.create(repoUriString);
+            final String projectId = teamParameters.get(SYSTEM_TEAM_PROJECT);
+            final String commit = teamParameters.get(BUILD_SOURCE_VERSION);
+            final String pushedBy = teamParameters.get(BUILD_REQUESTED_FOR);
+            final Integer pullRequestId = determinePullRequestId(teamParameters);
+            final CommitParameterAction action;
+            if (pullRequestId != null) {
+                final PullRequestMergeCommitCreatedEventArgs args = new PullRequestMergeCommitCreatedEventArgs();
+                args.collectionUri = collectionUri;
+                args.repoUri = repoUri;
+                args.projectId = projectId;
+                args.commit = commit;
+                args.pushedBy = pushedBy;
+                args.pullRequestId = pullRequestId;
+                args.iterationId = -1 /* TODO: the pull request iteration ID is missing! */;
+                action = new PullRequestParameterAction(args);
+            }
+            else {
+                final GitCodePushedEventArgs args = new GitCodePushedEventArgs();
+                args.collectionUri = collectionUri;
+                args.repoUri = repoUri;
+                args.projectId = projectId;
+                args.commit = commit;
+                args.pushedBy = pushedBy;
+                action = new CommitParameterAction(args);
+            }
+            actions.add(action);
+        }
+
+        return innerPerform(project, delay, actions);
+    }
+
+    static Integer determinePullRequestId(final HashMap<String, String> teamParameters) {
+        Integer pullRequestId = null;
+        if (teamParameters.containsKey(BUILD_SOURCE_BRANCH)) {
+            final String sourceBranch = teamParameters.get(BUILD_SOURCE_BRANCH);
+            if (sourceBranch.startsWith(REFS_PULL_SLASH)) {
+                final String idSlashMerge = sourceBranch.substring(REFS_PULL_SLASH_LENGTH);
+                final int nextSlash = idSlashMerge.indexOf('/');
+                if (nextSlash > 0) {
+                    final String pullRequestIdString = idSlashMerge.substring(0, nextSlash);
+                    pullRequestId = Integer.valueOf(pullRequestIdString, 10);
+                }
+            }
+        }
+        return pullRequestId;
     }
 }
