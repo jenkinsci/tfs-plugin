a3c8d03ad9f9ae21f71f40d72b833ba777eef504
Oli Dagenais <olivida@microsoft.com>
2016-08-02 15:13:54 -0400

    Implement GitPullRequestMerged

--- src/main/java/hudson/plugins/tfs/model/GitPullRequestMergedEvent.java
+++ src/main/java/hudson/plugins/tfs/model/GitPullRequestMergedEvent.java
@@ -1,13 +1,19 @@
 package hudson.plugins.tfs.model;
 
+import hudson.plugins.git.GitStatus;
+import hudson.plugins.tfs.PullRequestParameterAction;
 import hudson.plugins.tfs.util.MediaType;
 import net.sf.json.JSONObject;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.util.List;
 
 public class GitPullRequestMergedEvent extends GitPushEvent {
+
+    private static final String GIT_PULLREQUEST_MERGED = "git.pullrequest.merged";
 
     public static class Factory implements AbstractHookEvent.Factory {
 
@@ -34,5 +40,44 @@ public class GitPullRequestMergedEvent extends GitPushEvent {
 
     public GitPullRequestMergedEvent(final JSONObject requestPayload) {
         super(requestPayload);
+    }
+
+    static String determineCreatedBy(final JSONObject resource) {
+        final JSONObject createdBy = resource.getJSONObject("createdBy");
+        final String result = createdBy.getString("displayName");
+        return result;
+    }
+
+    @Override
+    public JSONObject perform(final JSONObject requestPayload) {
+        final PullRequestMergeCommitCreatedEventArgs args = decodeGitPullRequestMerged(requestPayload);
+        final PullRequestParameterAction parameterAction = new PullRequestParameterAction(args);
+        final List<GitStatus.ResponseContributor> contributors = pollOrQueueFromEvent(args, parameterAction, false);
+        final JSONObject response = fromResponseContributors(contributors);
+        return response;
+    }
+
+    static PullRequestMergeCommitCreatedEventArgs decodeGitPullRequestMerged(final JSONObject gitPullRequestMergedJson) {
+        assertEquals(gitPullRequestMergedJson, EVENT_TYPE, GIT_PULLREQUEST_MERGED);
+        final JSONObject resource = gitPullRequestMergedJson.getJSONObject(RESOURCE);
+        final JSONObject repository = resource.getJSONObject(REPOSITORY);
+        final URI collectionUri = determineCollectionUri(repository);
+        final String repoUriString = repository.getString(REMOTE_URL);
+        final URI repoUri = URI.create(repoUriString);
+        final String projectId = determineProjectId(repository);
+        final String repoId = repository.getString(NAME);
+        final String commit = determineCommit(resource);
+        final String pushedBy = determineCreatedBy(resource);
+        final int pullRequestId = resource.getInt("pullRequestId");
+
+        final PullRequestMergeCommitCreatedEventArgs args = new PullRequestMergeCommitCreatedEventArgs();
+        args.collectionUri = collectionUri;
+        args.repoUri = repoUri;
+        args.projectId = projectId;
+        args.repoId = repoId;
+        args.commit = commit;
+        args.pushedBy = pushedBy;
+        args.pullRequestId = pullRequestId;
+        return args;
     }
 }
--- src/test/java/hudson/plugins/tfs/model/GitPullRequestMergedEventTest.java
+++ src/test/java/hudson/plugins/tfs/model/GitPullRequestMergedEventTest.java
@@ -1,11 +1,31 @@
 package hudson.plugins.tfs.model;
 
+import net.sf.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.net.URI;
 
 /**
  * A class to test {@link GitPullRequestMergedEvent}.
  */
 public class GitPullRequestMergedEventTest {
 
+    @Test
+    public void decodeGitPullRequestMerged() throws Exception {
+        final GitPullRequestMergedEvent.Factory factory = new GitPullRequestMergedEvent.Factory();
+        final String inputString = factory.getSampleRequestPayload();
+        final JSONObject input = JSONObject.fromObject(inputString);
+
+        final PullRequestMergeCommitCreatedEventArgs actual = GitPullRequestMergedEvent.decodeGitPullRequestMerged(input);
+
+        Assert.assertEquals(URI.create("https://fabrikam.visualstudio.com/DefaultCollection/"), actual.collectionUri);
+        Assert.assertEquals(URI.create("https://fabrikam.visualstudio.com/DefaultCollection/_git/Fabrikam"), actual.repoUri);
+        Assert.assertEquals("Fabrikam", actual.projectId);
+        Assert.assertEquals("Fabrikam", actual.repoId);
+        Assert.assertEquals("53d54ac915144006c2c9e90d2c7d3880920db49c", actual.commit);
+        Assert.assertEquals("Jamal Hartnett", actual.pushedBy);
+        Assert.assertEquals(1, actual.pullRequestId);
+        Assert.assertEquals(-1, actual.iterationId);
+    }
 }
