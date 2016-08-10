79fb08d9fc57a683306214bcd1257ace314865fe
Oli Dagenais <olivida@microsoft.com>
2016-08-10 15:26:48 -0400

    Remove stuff only used in tests

--- src/test/java/hudson/plugins/tfs/model/GitCodePushedEventArgsTest.java
+++ src/test/java/hudson/plugins/tfs/model/GitCodePushedEventArgsTest.java
@@ -8,23 +8,4 @@ import org.junit.Test;
  */
 public class GitCodePushedEventArgsTest {
 
-    @Test public void fromJsonString_typical() throws Exception {
-
-        final GitCodePushedEventArgs actual = GitCodePushedEventArgs.fromJsonString(GitCodePushedEventArgs.SAMPLE_REQUEST_PAYLOAD);
-
-        Assert.assertEquals("https://fabrikam-fiber-inc.visualstudio.com", actual.collectionUri.toString());
-        Assert.assertEquals("https://fabrikam-fiber-inc.visualstudio.com/Personal/_git/olivida.tfs-plugin", actual.repoUri.toString());
-        Assert.assertEquals("Personal", actual.projectId);
-        Assert.assertEquals("olivida.tfs-plugin", actual.repoId);
-        Assert.assertEquals("6a23fc7afec31f0a14bade6544bed4f16492e6d2", actual.commit);
-        Assert.assertEquals("olivida", actual.pushedBy);
-
-        final WorkItem firstWorkItem = actual.workItems.get(0);
-        final Link htmlLink = firstWorkItem._links.get("html");
-        Assert.assertEquals("https://fabrikam-fiber-inc.visualstudio.com/web/wi.aspx?pcguid=d81542e4-cdfa-4333-b082-1ae2d6c3ad16&id=297", htmlLink.href.toString());
-
-        final Object systemTitle = firstWorkItem.fields.get("System_Title");
-        Assert.assertEquals("Customer can sign in using their Microsoft Account", systemTitle);
-    }
-
 }
