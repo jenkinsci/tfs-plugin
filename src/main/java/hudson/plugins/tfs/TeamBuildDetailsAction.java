212dadf8a972e94a13e014342f77b79cc7628a03
Oli Dagenais <olivida@microsoft.com>
2016-08-10 22:00:49 -0400

    Add summary view and supporting code in action

--- src/main/java/hudson/plugins/tfs/TeamBuildDetailsAction.java
+++ src/main/java/hudson/plugins/tfs/TeamBuildDetailsAction.java
@@ -1,9 +1,13 @@
 package hudson.plugins.tfs;
 
 import hudson.model.Action;
+import hudson.plugins.tfs.util.QueryString;
+import hudson.plugins.tfs.util.UriHelper;
+import org.kohsuke.stapler.export.Exported;
 import org.kohsuke.stapler.export.ExportedBean;
 
 import java.io.Serializable;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,8 +19,30 @@ public class TeamBuildDetailsAction implements Action, Serializable {
     private static final long serialVersionUID = 1L;
 
     public Map<String, String> buildVariables = new HashMap<String, String>();
+    public String buildUrl;
 
     public TeamBuildDetailsAction() {
+
+    }
+
+    public TeamBuildDetailsAction(final Map<String, String> buildVariables) {
+        this.buildVariables = new HashMap<String, String>(buildVariables);
+        this.buildUrl = determineBuildUrl(buildVariables).toString();
+    }
+
+    static URI determineBuildUrl(final Map<String, String> buildVariables) {
+        // TODO: eventually call the build REST API to obtain the proper web URL
+        final String collectionUri = buildVariables.get("System.TeamFoundationCollectionUri");
+        final String projectName = buildVariables.get("System.TeamProject");
+        final String buildId = buildVariables.get("Build.BuildId");
+        final QueryString query = new QueryString("buildId", buildId);
+        final URI result = UriHelper.join(
+                collectionUri,
+                projectName,
+                "_build",
+                "index",
+                query);
+        return result;
 
     }
 
@@ -34,5 +60,22 @@ public class TeamBuildDetailsAction implements Action, Serializable {
     @Override
     public String getUrlName() {
         return "team-build";
+    }
+
+    // the following methods are called from this/summary.jelly
+
+    @Exported
+    public String getBuildNumber() {
+        return buildVariables.get("Build.BuildNumber");
+    }
+
+    @Exported
+    public String getBuildDefinitionName() {
+        return buildVariables.get("Build.DefinitionName");
+    }
+
+    @Exported
+    public String getBuildUrl() {
+        return buildUrl;
     }
 }
