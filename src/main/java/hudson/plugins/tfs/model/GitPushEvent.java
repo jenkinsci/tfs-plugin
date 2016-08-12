a558e6969e55b4de1d74e45f7cd29c95b31c23e9
Oli Dagenais <olivida@microsoft.com>
2016-08-12 10:01:14 -0400

    Implement new perform() overload & supporting code

--- src/main/java/hudson/plugins/tfs/model/GitPushEvent.java
+++ src/main/java/hudson/plugins/tfs/model/GitPushEvent.java
@@ -2,11 +2,17 @@ package hudson.plugins.tfs.model;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.teamfoundation.core.webapi.model.TeamProjectReference;
+import com.microsoft.teamfoundation.sourcecontrol.webapi.model.GitCommitRef;
+import com.microsoft.teamfoundation.sourcecontrol.webapi.model.GitPush;
+import com.microsoft.teamfoundation.sourcecontrol.webapi.model.GitRepository;
+import com.microsoft.visualstudio.services.webapi.model.IdentityRef;
 import hudson.plugins.git.GitStatus;
 import hudson.plugins.tfs.CommitParameterAction;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
@@ -45,8 +51,19 @@ public class GitPushEvent extends AbstractHookEvent {
 
     @Override
     public JSONObject perform(final ObjectMapper mapper, final JsonParser resourceParser) {
-        // TODO: implement this overload
-        return null;
+        final GitPush gitPush;
+        try {
+            gitPush = mapper.readValue(resourceParser, GitPush.class);
+        }
+        catch (final IOException e) {
+            throw new Error(e);
+        }
+
+        final GitCodePushedEventArgs args = decodeGitPush(gitPush);
+        final CommitParameterAction parameterAction = new CommitParameterAction(args);
+        final List<GitStatus.ResponseContributor> contributors = pollOrQueueFromEvent(args, parameterAction, false);
+        final JSONObject response = fromResponseContributors(contributors);
+        return response;
     }
 
     static void assertEquals(final JSONObject jsonObject, final String key, final String expectedValue) {
@@ -91,9 +108,21 @@ public class GitPushEvent extends AbstractHookEvent {
         return determineCollectionUri(repoApiUri);
     }
 
+    static URI determineCollectionUri(final GitRepository repository) {
+        final String repoApiUrlString = repository.getUrl();
+        final URI repoApiUri = URI.create(repoApiUrlString);
+        return determineCollectionUri(repoApiUri);
+    }
+
     static String determineProjectId(final JSONObject repository) {
         final JSONObject project = repository.getJSONObject("project");
         final String result = project.getString("name");
+        return result;
+    }
+
+    static String determineProjectId(final GitRepository repository) {
+        final TeamProjectReference project = repository.getProject();
+        final String result = project.getName();
         return result;
     }
 
@@ -106,9 +135,24 @@ public class GitPushEvent extends AbstractHookEvent {
         return commitObject.getString("commitId");
     }
 
+    static String determineCommit(final GitPush gitPush) {
+        final List<GitCommitRef> commits = gitPush.getCommits();
+        if (commits.size() < 1) {
+            throw new IllegalArgumentException("No commits found");
+        }
+        final GitCommitRef commit = commits.get(0);
+        return commit.getCommitId();
+    }
+
     static String determinePushedBy(final JSONObject resource) {
         final JSONObject pushedBy = resource.getJSONObject("pushedBy");
         final String result = pushedBy.getString("displayName");
+        return result;
+    }
+
+    static String determinePushedBy(final GitPush gitPush) {
+        final IdentityRef pushedBy = gitPush.getPushedBy();
+        final String result = pushedBy.getDisplayName();
         return result;
     }
 
@@ -123,6 +167,26 @@ public class GitPushEvent extends AbstractHookEvent {
         final String repoId = repository.getString(NAME);
         final String commit = determineCommit(resource);
         final String pushedBy = determinePushedBy(resource);
+
+        final GitCodePushedEventArgs args = new GitCodePushedEventArgs();
+        args.collectionUri = collectionUri;
+        args.repoUri = repoUri;
+        args.projectId = projectId;
+        args.repoId = repoId;
+        args.commit = commit;
+        args.pushedBy = pushedBy;
+        return args;
+    }
+
+    static GitCodePushedEventArgs decodeGitPush(final GitPush gitPush) {
+        final GitRepository repository = gitPush.getRepository();
+        final URI collectionUri = determineCollectionUri(repository);
+        final String repoUriString = repository.getRemoteUrl();
+        final URI repoUri = URI.create(repoUriString);
+        final String projectId = determineProjectId(repository);
+        final String repoId = repository.getName();
+        final String commit = determineCommit(gitPush);
+        final String pushedBy = determinePushedBy(gitPush);
 
         final GitCodePushedEventArgs args = new GitCodePushedEventArgs();
         args.collectionUri = collectionUri;
--- src/main/java/hudson/plugins/tfs/model/PingHookEvent.java
+++ src/main/java/hudson/plugins/tfs/model/PingHookEvent.java
@@ -1,8 +1,14 @@
 package hudson.plugins.tfs.model;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.sf.json.JSONObject;
+
+import java.io.IOException;
+import java.io.StringWriter;
 
 public class PingHookEvent extends AbstractHookEvent {
 
@@ -15,7 +21,11 @@ public class PingHookEvent extends AbstractHookEvent {
         @Override
         public String getSampleRequestPayload() {
             return "{\n" +
-                    "    \"message\": \"Hello, world!\"\n" +
+                    "    \"eventType\": \"ping\",\n" +
+                    "    \"resource\":\n" +
+                    "    {\n" +
+                    "        \"message\": \"Hello, world!\"\n" +
+                    "    }\n" +
                     "}";
         }
     }
@@ -27,6 +37,18 @@ public class PingHookEvent extends AbstractHookEvent {
 
     @Override
     public JSONObject perform(final ObjectMapper mapper, final JsonParser resourceParser) {
-        return null;
+        final TreeNode treeNode;
+        try {
+            treeNode = mapper.readTree(resourceParser);
+            final JsonFactory factory = mapper.getFactory();
+            final StringWriter sw = new StringWriter();
+            final JsonGenerator generator = factory.createGenerator(sw);
+            mapper.writeTree(generator, treeNode);
+            final String jsonString = sw.toString();
+            return JSONObject.fromObject(jsonString);
+        }
+        catch (final IOException e) {
+            throw new Error(e);
+        }
     }
 }
