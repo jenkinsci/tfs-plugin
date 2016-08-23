bef3077c90cd5ba4ae704f5989373e33076fd5ba
Oli Dagenais <olivida@microsoft.com>
2016-08-23 13:54:00 -0400

    Detect most corrupted or missing folder mappings

--- src/main/java/hudson/plugins/tfs/actions/CheckoutAction.java
+++ src/main/java/hudson/plugins/tfs/actions/CheckoutAction.java
@@ -3,14 +3,17 @@ package hudson.plugins.tfs.actions;
 import com.microsoft.tfs.core.clients.versioncontrol.specs.version.DateVersionSpec;
 import com.microsoft.tfs.core.clients.versioncontrol.specs.version.VersionSpec;
 import hudson.FilePath;
+import hudson.model.TaskListener;
 import hudson.plugins.tfs.commands.RemoteChangesetVersionCommand;
 import hudson.plugins.tfs.model.ChangeSet;
+import hudson.plugins.tfs.model.MockableVersionControlClient;
 import hudson.plugins.tfs.model.Project;
 import hudson.plugins.tfs.model.Server;
 import hudson.plugins.tfs.model.Workspace;
 import hudson.plugins.tfs.model.Workspaces;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -83,15 +86,33 @@ public class CheckoutAction {
         final Project project = server.getProject(projectPath);
         final FilePath localFolderPath = workspacePath.child(localFolder);
         final String localPath = localFolderPath.getRemote();
+        final TaskListener listener = server.getListener();
+        final PrintStream logger = listener.getLogger();
 
-        if (workspaces.exists(workspaceName) && !useUpdate) {
-            Workspace workspace = workspaces.getWorkspace(workspaceName);
-            workspaces.deleteWorkspace(workspace);
+        boolean shouldDelete = false;
+        if (workspaces.exists(workspaceName)) {
+            boolean localFolderExists = true;
+            final boolean isMapped = workspaces.isMapped(localPath);
+            if (!isMapped) {
+                logger.println("Warning: Although the server thinks the workspace exists, no mapping was found.");
+            }
+            else {
+                localFolderExists = localFolderPath.exists();
+                if (!localFolderExists) {
+                    logger.println("Warning: The local folder is missing.");
+                }
+
+            }
+            if (!localFolderExists || !isMapped || !useUpdate) {
+                shouldDelete = true;
+                Workspace workspace = workspaces.getWorkspace(workspaceName);
+                workspaces.deleteWorkspace(workspace);
+            }
         }
 
         Workspace workspace;
         if (! workspaces.exists(workspaceName)) {
-            if (!useUpdate && localFolderPath.exists()) {
+            if (shouldDelete && localFolderPath.exists()) {
                 localFolderPath.deleteContents();
             }
             final String serverPath = project.getProjectPath();
