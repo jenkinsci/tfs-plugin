99420ea74f1b2143acd58c4a94cdfcc98cb99ce6
Oli Dagenais <olivida@microsoft.com>
2016-08-25 11:12:32 -0400

    Wire up proxy credentials if available

--- src/main/java/hudson/plugins/tfs/model/ModernHTTPClientFactory.java
+++ src/main/java/hudson/plugins/tfs/model/ModernHTTPClientFactory.java
@@ -2,9 +2,13 @@ package hudson.plugins.tfs.model;
 
 import com.microsoft.tfs.core.config.ConnectionInstanceData;
 import com.microsoft.tfs.core.config.httpclient.DefaultHTTPClientFactory;
+import com.microsoft.tfs.core.httpclient.DefaultNTCredentials;
 import com.microsoft.tfs.core.httpclient.HostConfiguration;
 import com.microsoft.tfs.core.httpclient.HttpClient;
 import com.microsoft.tfs.core.httpclient.HttpState;
+import com.microsoft.tfs.core.httpclient.UsernamePasswordCredentials;
+import com.microsoft.tfs.core.httpclient.auth.AuthScope;
+import hudson.util.Secret;
 
 public class ModernHTTPClientFactory extends DefaultHTTPClientFactory {
 
@@ -22,5 +26,20 @@ public class ModernHTTPClientFactory extends DefaultHTTPClientFactory {
     @Override
     public void configureClientProxy(final HttpClient httpClient, final HostConfiguration hostConfiguration,final HttpState httpState, final ConnectionInstanceData connectionInstanceData) {
         hostConfiguration.setProxyHost(proxyHost);
+
+        final String proxyUser = proxyHost.getProxyUser();
+        final Secret proxySecret = proxyHost.getProxySecret();
+        if (proxyUser != null && proxySecret != null) {
+            httpState.setProxyCredentials(
+                    AuthScope.ANY,
+                    new UsernamePasswordCredentials(proxyUser, proxySecret.getPlainText())
+            );
+        }
+        else {
+            httpState.setProxyCredentials(
+                    AuthScope.ANY,
+                    new DefaultNTCredentials()
+            );
+        }
     }
 }
