2f5e013371da1d76fb03a565ab6cc09bfa0ca884
Oli Dagenais <olivida@microsoft.com>
2016-09-02 17:15:35 -0400

    Remove redundant help link

--- tfs/src/main/resources/hudson/plugins/tfs/TeamCollectResultsPostBuildAction/config.groovy
+++ tfs/src/main/resources/hudson/plugins/tfs/TeamCollectResultsPostBuildAction/config.groovy
@@ -3,8 +3,7 @@ package hudson.plugins.tfs.TeamCollectResultsPostBuildAction;
 def f = namespace(lib.FormTagLib);
 
 // this would look/feel nicer if the Add button was a drop-down list, like for build steps
-f.entry(title: _("Build results to collect"),
-        help: descriptor.getHelpFile()) {
+f.entry(title: _("Build results to collect")) {
     f.repeatableProperty(field: "requestedResults") {
         f.entry {
             div(align: "right") {
