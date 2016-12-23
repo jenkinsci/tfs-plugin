02df89fef3304400489cc8489afcd9cfe07b6647
Olivier Dagenais <olivier.dagenais@gmail.com>
2016-12-22 22:56:15 -0500

    Add EventScopeTest skeleton

---
+++ tfs/src/test/java/hudson/plugins/tfs/model/servicehooks/EventScopeTest.java
@@ -0,0 +1,11 @@
+package hudson.plugins.tfs.model.servicehooks;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * A class to test {@link EventScope}.
+ */
+public class EventScopeTest {
+
+}
