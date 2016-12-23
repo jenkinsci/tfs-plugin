35ad535162803a49b7f1268a8b10642e5889bd35
Olivier Dagenais <olivier.dagenais@gmail.com>
2016-12-22 23:18:58 -0500

    TDD: Event can have a lowercase value for "scope"
    JENKINS-40155 reported a failed deserialization due to a case mismatch
    in the value of "scope", so added a factory method used by Jackson.

--- tfs/src/main/java/hudson/plugins/tfs/model/servicehooks/EventScope.java
+++ tfs/src/main/java/hudson/plugins/tfs/model/servicehooks/EventScope.java
@@ -1,5 +1,11 @@
 package hudson.plugins.tfs.model.servicehooks;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
 public enum EventScope {
     /**
      * No input scope specified.
@@ -30,4 +36,28 @@ public enum EventScope {
      * Deployment scope.
      */
     Deployment,
+
+    ;
+
+    private static final Map<String, EventScope> CASE_INSENSITIVE_LOOKUP;
+
+    static {
+        final Map<String, EventScope> map = new TreeMap<String, EventScope>(String.CASE_INSENSITIVE_ORDER);
+        for (final EventScope value : EventScope.values()) {
+            map.put(value.name(), value);
+        }
+        CASE_INSENSITIVE_LOOKUP = Collections.unmodifiableMap(map);
+    }
+
+    @SuppressWarnings("unused" /* Invoked by Jackson via @JsonCreator */)
+    @JsonCreator
+    public static EventScope caseInsensitiveValueOf(final String name) {
+        if (name == null) {
+            throw new NullPointerException("Name is null");
+        }
+        if (!CASE_INSENSITIVE_LOOKUP.containsKey(name)) {
+            throw new IllegalArgumentException("No enum constant " + name);
+        }
+        return CASE_INSENSITIVE_LOOKUP.get(name);
+    }
 }
