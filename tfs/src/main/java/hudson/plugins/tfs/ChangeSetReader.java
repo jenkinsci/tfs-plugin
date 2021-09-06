60bd43623093a56749a2a1625e1aabd6a855c984
Tomasz Swiecicki <tomasz.swiecicki@bakermckenzie.com>
2021-09-06 22:09:01 +0200

    fixed Digester

--- tfs/src/main/java/hudson/plugins/tfs/ChangeSetReader.java
+++ tfs/src/main/java/hudson/plugins/tfs/ChangeSetReader.java
@@ -8,16 +8,17 @@ import java.io.Reader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
 
 import hudson.model.Run;
 import hudson.scm.RepositoryBrowser;
-import org.apache.commons.digester.Digester;
-import org.xml.sax.SAXException;
 
+import org.xml.sax.SAXException;
+import org.apache.commons.digester3.Digester;
 import hudson.plugins.tfs.model.ChangeLogSet;
 import hudson.plugins.tfs.model.ChangeSet;
 import hudson.scm.ChangeLogParser;
-import hudson.util.Digester2;
+
 
 /**
  * TeamFoundation change log reader.
@@ -36,7 +37,8 @@ public class ChangeSetReader extends ChangeLogParser {
     /** Performs the actual parsing. */
     public ChangeLogSet parse(final Run build, final RepositoryBrowser<?> browser, final Reader reader) throws IOException, SAXException {
         List<ChangeSet> changesetList = new ArrayList<ChangeSet>();
-        Digester digester = new Digester2();
+       // Digester digester = new Digester2();
+        Digester digester = createDigester(true);
         digester.push(changesetList);
 
         digester.addObjectCreate("*/changeset", ChangeSet.class);
@@ -56,4 +58,21 @@ public class ChangeSetReader extends ChangeLogParser {
 
         return new ChangeLogSet(build, browser, changesetList);
     }
+         /** Creates a Digester. */
+       public static Digester createDigester(final boolean secure) throws SAXException {
+        Digester digester = new Digester();
+        if (secure) {
+            digester.setXIncludeAware(false);
+            try {
+                digester.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+                digester.setFeature("http://xml.org/sax/features/external-general-entities", false);
+                digester.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+                digester.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+            } catch (ParserConfigurationException ex) {
+                throw new SAXException("Failed to securely configure xml digester parser", ex);
+            }
+        }
+        return digester;
+    }
+
 }
